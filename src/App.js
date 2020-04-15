d133620846a2775e13a6ac18aa736a7b90532928
Mohit <mohityd10@gmail.com>
2020-04-15 14:04:04 +0530

    CSS changes

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ function App() {
     return (
     <div className="App">
         <h1>How fast do you type?</h1>
+        <h4>Time remaining: {timeRemaining}</h4>
         <textarea
             ref={textBoxRef}
             onChange={handleChange}
             value={textInput}
             disabled={!isTimeRunning}
         />
-        <h4>Time remaining: {timeRemaining}</h4>
         <button
             onClick={startGame}
             disabled={isTimeRunning}
