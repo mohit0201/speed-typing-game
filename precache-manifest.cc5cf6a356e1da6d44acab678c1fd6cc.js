10226ef51d930b13ce41d110c30e985b77858abf
Mohit <mohityd10@gmail.com>
2020-04-15 13:42:18 +0530

    Updates

---
+++ precache-manifest.cc5cf6a356e1da6d44acab678c1fd6cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4d6b1f78ce66b94523480f5e4f616a",
+    "url": "/speed-typing-game/index.html"
+  },
+  {
+    "revision": "c5d743ff271c05a35b30",
+    "url": "/speed-typing-game/static/css/main.4ab5477d.chunk.css"
+  },
+  {
+    "revision": "d9d4c440bfed1f2a8865",
+    "url": "/speed-typing-game/static/js/2.e71f9331.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speed-typing-game/static/js/2.e71f9331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d743ff271c05a35b30",
+    "url": "/speed-typing-game/static/js/main.6b36b4ec.chunk.js"
+  },
+  {
+    "revision": "93c861f9f855248d6a45",
+    "url": "/speed-typing-game/static/js/runtime-main.97ec1b5f.js"
+  }
+]);
